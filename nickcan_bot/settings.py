678d90c147cfb8d4be2aba7475c97d6eaf1a40b5
nick411077 <nick411077@gmail.com>
2020-03-01 00:27:25 +0800

    Update commit

--- nickcan_bot/settings.py
+++ nickcan_bot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ibn4@o1y%_7kuq+$928g)=dhe#p@9dpa2=9gcn_p_7vh53#mp('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
