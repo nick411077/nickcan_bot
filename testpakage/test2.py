54a0e2232a1d53bdfd25d3e1d7ae2e160a2eca02
nick411077 <nick411077@gmail.com>
2020-03-06 16:26:05 +0800

    Update commit

--- testpakage/test2.py
+++ testpakage/test2.py
@@ -1,28 +1,52 @@
 from threading import Event, Thread
-from time import sleep
+import time
+import twitch
 
-singal = Event()
+usernames = 'latteda_', 'twnickcan', 'miyaaoba'
+def main():
+    starttime = time.time()
 
-
-def func(singal):
     while True:
-        singal.wait()  # wait for singal turn on
-        print("do job")
-        print("job end")
-        singal.clear()  # turn off singal
+        mainloop()
+        time.sleep(300.0 - (time.time() - starttime % 300.0))
+def mainloop():
+    print("running")
+    global streams_current
+
+    print(f"Current list of stream ids: {list(streams_current.keys())}")
+    stream_new = get_streams()
+
+    streamids_status = compare_streams(streams_current, streams_new)
+
+    announce_streams(streamids_status, streams_current, streams_new)
+
+    streams_current = streams_new
+
+def get_streams():
+    streams = {}
+    ClientID = 'g3v9rj6v0t5cuthn57g3s9sd1sngmz'
+    width = 640
+    height = 360
+    helix = twitch.Helix(ClientID)
+
+    for user in helix.users(usernames):
+        try:
+            user.stream.type
+        except twitch.helix.resources.streams.StreamNotFound:
+            print('None')
+            continue
+        print(user.stream.data)
+
+        if res['data'] != []:
+            stream_id = res['data'][0]['id']
+            streams[stream_id] = {}
+            streams[stream_id]['username'] = user
+            streams[stream_id]['url'] = 'https://www.twitch.tv/' + user
+            streams[stream_id]['game'] = res['data'][0]['user_name']
+            streams[stream_id]['status'] = res['data'][0]['title']
+            streams[stream_id]['preview_l'] = res['data'][0]['thumbnail_url']
+            streams[stream_id]['preview_l'] = streams[stream_id]['preview_l'].replace('{width}', '640')
+            streams[stream_id]['preview_l'] = streams[stream_id]['preview_l'].replace('{height}', '360')
+            print(streams[stream_id]['preview_l'.format(width, height)])
 
 
-t = Thread(target=func, args=[singal])
-t.start()
-
-print("1st time")
-singal.set()  # turn on singal
-sleep(3)
-
-print("2nd time")
-singal.set()
-sleep(3)
-
-print("3rd time")
-singal.set()
-sleep(3)
